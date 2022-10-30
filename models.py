841bc9d05370c99c3503c00251595b565fa28727
Tejas K Jaiswal <tejas.kjaiswal@yahoo.com>
2022-10-30 13:16:43 +0530

    Work has been completed as intended

--- models.py
+++ models.py
@@ -15,3 +15,28 @@ def register_user(data):
     count, result = insert_data(query, fname, lname, email, username, password)
 
     return count, result
+
+def login_user(data):
+    query = f"""
+        SELECT user_id, user_uuid, username
+        FROM public.users
+        WHERE username=%s AND password=crypt(%s, password)
+        LIMIT 1
+    """
+    username = data['username']
+    password = data['password']
+
+    count, result = get_data_as_dict(query, username, password)
+
+    return count, result
+
+def get_user_uuid(user_uuid):
+    query = f"""
+        SELECT user_uuid
+        FROM public.users
+        WHERE user_uuid=%s
+        LIMIT 1
+    """
+    count, result = get_data_as_dict(query, user_uuid)
+
+    return count, result
--- movies_summary.py
+++ movies_summary.py
@@ -1,10 +1,12 @@
 from flask import current_app, Blueprint, jsonify, render_template, request
 
 from .apis.imdb_api import get_movie_summary
+from .authentication import token_required
 
 movies_summary = Blueprint('movies_summary', __name__, template_folder='templates/movies', static_folder='static')
 
 @movies_summary.route('/get_movie_data', methods=['GET'])
+@token_required
 def get_movie_data():
     if not request.args.get('q'):
         current_app.logger.error("No query provided")
@@ -15,5 +17,6 @@ def get_movie_data():
     }
 
     movies_json = get_movie_summary(querystring=query)
+    # movies_json = ""
 
     return movies_json, 200
